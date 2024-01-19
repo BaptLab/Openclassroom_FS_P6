032205a4efcbaeb607cd69459c86ab808d2dcb2c
BaptLab <baptiste.labaune@gmail.com>
2024-01-19 14:31:58 +0100

    all models and payloads done

--- back/src/main/java/com/openclassrooms/models/Article.java
+++ back/src/main/java/com/openclassrooms/models/Article.java
@@ -36,4 +36,53 @@ public class Article {
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	
 }
---
+++ back/src/main/java/com/openclassrooms/models/Theme.java
@@ -0,0 +1,49 @@
+package com.openclassrooms.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Entity
+@Table(name = "THEMES")
+@EntityListeners(AuditingEntityListener.class)
+public class Theme {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "description may not be null")
+	private String description;
+	
+	@NotNull(message = "title may not be null")
+	private String title;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
