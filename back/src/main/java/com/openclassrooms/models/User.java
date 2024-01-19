4c48822f667896aeadddc04e35fb3b455e0b9929
BaptLab <baptiste.labaune@gmail.com>
2024-01-19 12:46:43 +0100

    User layer done

--- back/src/main/java/com/openclassrooms/models/User.java
+++ back/src/main/java/com/openclassrooms/models/User.java
@@ -3,25 +3,84 @@ package com.openclassrooms.models;
 import java.time.LocalDateTime;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
-import lombok.NonNull;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 
 @Entity
 @Table(name = "USERS")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotNull(message = "email may not be null")
 	private String email;
+	
+	@NotNull(message = "username may not be null")
 	private String username;
+	
+	@NotNull(message = "password may not be null")
 	private String password;
 	
+	@CreatedDate
 	@Column(name = "created_at")
 	private LocalDateTime createdAt;
 	
+	@UpdateTimestamp
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
 	
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
 }
