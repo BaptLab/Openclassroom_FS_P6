5a44671f974f1f31cdd5730e9b6c60f52ce1f89a
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2024-01-22 12:20:45 +0100

    corrections

--- ressources/sql/script.sql
+++ ressources/sql/script.sql
@@ -11,7 +11,6 @@ CREATE TABLE `ARTICLES` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `title` VARCHAR(400),
   `content` TEXT,
-  `theme` VARCHAR(40),
   `user_id` INT,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -22,6 +21,14 @@ CREATE TABLE `THEMES` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `title` VARCHAR(50),
   `description` VARCHAR(800)
+);
+
+CREATE TABLE `USER_THEMES` (
+  `user_id` INT,
+  `theme_id` INT,
+  PRIMARY KEY (`user_id`, `theme_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`),
+  FOREIGN KEY (`theme_id`) REFERENCES `THEMES` (`id`)
 );
 
 CREATE TABLE `COMMENTS` (
@@ -41,11 +48,11 @@ VALUES
   ('bob_jones', 'bob@example.com', 'hashed_password_3');
 
 -- Insert fake values into ARTICLES table
-INSERT INTO `ARTICLES` (`title`, `content`, `theme`, `user_id`)
+INSERT INTO `ARTICLES` (`title`, `content`, `user_id`)
 VALUES
-  ('Introduction to SQL', 'This is an introductory article about SQL.', 'Database', 1),
-  ('Building a Spring Boot Application', 'Learn how to build a web app with Spring Boot.', 'Java', 2),
-  ('The Art of Cooking', 'Explore the world of culinary arts.', 'Cooking', 3);
+  ('Introduction to SQL', 'This is an introductory article about SQL.', 1),
+  ('Building a Spring Boot Application', 'Learn how to build a web app with Spring Boot.', 2),
+  ('The Art of Cooking', 'Explore the world of culinary arts.', 3);
 
 -- Insert fake values into THEMES table
 INSERT INTO `THEMES` (`title`, `description`)
@@ -53,6 +60,13 @@ VALUES
   ('Database', 'Discussions about database systems and management.'),
   ('Java', 'Topics related to Java programming language.'),
   ('Cooking', 'Recipes, tips, and discussions about cooking.');
+
+-- Insert fake values into USER_THEMES table
+INSERT INTO `USER_THEMES` (`user_id`, `theme_id`)
+VALUES
+  (1, 1),  -- User 1 subscribed to Theme 1
+  (2, 2),  -- User 2 subscribed to Theme 2
+  (3, 3);  -- User 3 subscribed to Theme 3
 
 -- Insert fake values into COMMENTS table
 INSERT INTO `COMMENTS` (`user_id`, `article_id`, `content`)
